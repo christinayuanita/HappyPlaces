e7605b16d765e73a53657038424e8ee6e3881845
Christina Yuanita <christinayuanita380@gmail.com>
2021-05-29 12:16:50 +0700

    create model and prepare database handler

--- app/src/main/java/com/example/happyplaces/activities/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/AddHappyPlaceActivity.kt
@@ -1,4 +1,4 @@
-package com.example.happyplaces
+package com.example.happyplaces.activities
 
 import android.Manifest
 import android.app.Activity
@@ -17,6 +17,7 @@ import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.example.happyplaces.R
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -33,8 +34,10 @@ import java.util.*
 class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
     private var cal = Calendar.getInstance()
-
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+    private var saveImageToInternalStorage: Uri? = null
+    private var mLatitude: Double = 0.0
+    private var mLongitude: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +59,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
 
         et_date.setOnClickListener(this)
         tv_add_image.setOnClickListener(this)
+        btn_save.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
@@ -79,6 +83,9 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                 }
                 pictureDialog.show()
             }
+            R.id.btn_save -> {
+
+            }
         }
     }
 
@@ -91,7 +98,7 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
                     try {
                         val selectedImageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
 
-                        val saveImageToInternalStorage = saveImageToInternalStorage(selectedImageBitmap)
+                        saveImageToInternalStorage = saveImageToInternalStorage(selectedImageBitmap)
 
                         Log.e("Saved image: " , "Path :: $saveImageToInternalStorage")
                         iv_place_image.setImageBitmap(selectedImageBitmap)
@@ -104,10 +111,12 @@ class AddHappyPlaceActivity : AppCompatActivity(), View.OnClickListener {
             else if(requestCode == CAMERA){
                 val thumbnail : Bitmap = data!!.extras!!.get("data") as Bitmap
 
-                val saveImageToInternalStorage = saveImageToInternalStorage(thumbnail)
+                saveImageToInternalStorage = saveImageToInternalStorage(thumbnail)
 
                 Log.e("Saved image: " , "Path :: $saveImageToInternalStorage")
                 iv_place_image.setImageBitmap(thumbnail)
+
+
             }
         }
     }
---
+++ app/src/main/java/com/example/happyplaces/database/DatabaseHandler.kt
@@ -0,0 +1,63 @@
+package com.example.happyplaces.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.happyplaces.models.HappyPlaceModel
+
+class DatabaseHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object{
+        private const val DATABASE_VERSION = 1
+        private const val DATABASE_NAME = "HappyPlacesDatabase"
+        private const val TABLE_HAPPY_PLACE = "HappyPlacesTable"
+
+        private const val KEY_ID = "_id"
+        private const val KEY_TITLE = "title"
+        private const val KEY_IMAGE = "image"
+        private const val KEY_DESCRIPTION = "description"
+        private const val KEY_DATE = "date"
+        private const val KEY_LOCATION = "location"
+        private const val KEY_LATITUDE = "latitude"
+        private const val KEY_LONGITUDE = "longitude"
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val CREATE_HAPPY_PLACE_TABLE = (
+            "CREATE TABLE " + TABLE_HAPPY_PLACE + "("
+            + KEY_ID + " INTEGER PRIMARY KEY,"
+            + KEY_TITLE + " TEXT,"
+            + KEY_IMAGE + " TEXT,"
+            + KEY_DESCRIPTION + " TEXT,"
+            + KEY_DATE + " TEXT,"
+            + KEY_LOCATION + " TEXT,"
+            + KEY_LATITUDE + " TEXT,"
+            + KEY_LONGITUDE + " TEXT)"
+        )
+        db?.execSQL(CREATE_HAPPY_PLACE_TABLE)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db!!.execSQL("DROP TABLE IF EXISTS $TABLE_HAPPY_PLACE")
+        onCreate(db)
+    }
+
+    fun addHappyPlace(happyPlace: HappyPlaceModel): Long{
+        val values = ContentValues()
+        values.put(KEY_TITLE, happyPlace.title)
+        values.put(KEY_IMAGE, happyPlace.image)
+        values.put(KEY_DESCRIPTION, happyPlace.description)
+        values.put(KEY_DATE, happyPlace.date)
+        values.put(KEY_LOCATION, happyPlace.location)
+        values.put(KEY_LATITUDE, happyPlace.latitude)
+        values.put(KEY_LONGITUDE, happyPlace.longitude)
+
+        val db = this.writableDatabase
+        val result = db.insert(TABLE_HAPPY_PLACE, null, values)
+        db.close()
+
+        return result
+
+
+}
