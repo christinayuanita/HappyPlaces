649b4b3e059d788ad329503754d4a441b241cd90
Christina Yuanita <christinayuanita380@gmail.com>
2021-05-29 22:59:03 +0700

    swipe to delete happy place

--- app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
+++ app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -11,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.happyplaces.R
 import com.example.happyplaces.activities.AddHappyPlaceActivity
 import com.example.happyplaces.activities.MainActivity
+import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.item_happy_place.view.*
 
@@ -51,6 +52,15 @@ open class HappyPlacesAdapter (private val context: Context, private val list: A
         intent.putExtra(MainActivity.EXTRA_PLACE_DETAILS, list[position])
         activity.startActivityForResult(intent, requestCode)
         notifyItemChanged(position)
+    }
+
+    fun removeAt(position: Int){
+        val dbHandler = DatabaseHandler(context)
+        val isDeleted = dbHandler.deleteHappyPlace(list[position])
+        if(isDeleted > 0){
+            list.removeAt(position)
+            notifyItemRemoved(position)
+        }
     }
 
     interface OnClickListener{
